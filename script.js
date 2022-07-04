1a2bed3b3dec47d16c42b69f6d3bb665d8fede5f
Joshua Matlock <jkmatlock1989@gmail.com>
2022-07-03 18:31:29 -0700

    Add inital CSS and Javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('#Box4');
+const Row = document.createElement('div');
+Row.classList.add('Row')
+container.append(Child)
+
+
+
+
+
+
+function Test()
+{
+    const container = document.querySelector('#Box4');
+    const Row = document.createElement('div');
+    Row.classList.add('Row')
+    container.append(Child)
+
+
+}
