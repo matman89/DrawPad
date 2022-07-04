677b04d9239e22ab04393fe8f59ea2e294b52084
Joshua Matlock <jkmatlock1989@gmail.com>
2022-07-03 21:26:23 -0700

    Add basic grid

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const container = document.querySelector('#Box4');
-const Row = document.createElement('div');
-Row.classList.add('Row')
-container.append(Child)
-
-
-
-
-
-
-function Test()
+function TenxTen()
 {
+    for(i=0; i < 10; i++) 
+    {
     const container = document.querySelector('#Box4');
     const Row = document.createElement('div');
-    Row.classList.add('Row')
-    container.append(Child)
-
-
+    Row.id = 'r' + i;
+    Row.classList.add('Row');
+    container.appendChild(Row)
+        for(j=0; j < 10; j++)
+        {
+        const rcontainer = document.querySelector(`#r${i}.Row`);
+        const Col = document.createElement('div');
+        Col.classList.add('Col');
+        rcontainer.appendChild(Col)
+        } 
+    }
 }
+
+TenxTen()
